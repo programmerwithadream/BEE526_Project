556ece8646d7b5c539865be12d7d23ab0aff75cc
Justin S <justinshyi123@gmail.com>
2023-05-11 13:32:44 -0700

    added tests to sram select, execute task signals

--- RPi/fpga_comm_test.cpp
+++ RPi/fpga_comm_test.cpp
@@ -13,6 +13,12 @@ constexpr uint8_t WRSR = 0x01;
 
 int spi_channel = 1; // SPI Channel (0 or 1)
 
+const int sram_select0 = 24;
+const int sram_select1 = 23;
+const int inst_valid = 17;
+const int job_done = 27;
+const int execute_task = 22;
+
 void write_byte(uint32_t address, uint8_t value) {
     uint8_t buffer[5] = {WRITE, static_cast<uint8_t>((address >> 16) & 0xFF), static_cast<uint8_t>((address >> 8) & 0xFF), static_cast<uint8_t>(address & 0xFF), value};
     wiringPiSPIDataRW(spi_channel, buffer, 5);
@@ -63,15 +69,64 @@ int main() {
         return 1;
     }
     
-    digitalWrite(10, HIGH);
+    // digitalWrite(10, HIGH);
     
-    int test_num = 5423;
-    uint8_t buffer[4];
-    buffer[0] = static_cast<uint8_t>((test_num >> 24) & 0xFF);
-    buffer[1] = static_cast<uint8_t>((test_num >> 16) & 0xFF);
-    buffer[2] = static_cast<uint8_t>((test_num >> 8) & 0xFF);
-    buffer[3] = static_cast<uint8_t>(test_num & 0xFF);
-    wiringPiSPIDataRW(spi_channel, buffer, 4);
+    // int test_num = 5423;
+    // uint8_t buffer[4];
+    // buffer[0] = static_cast<uint8_t>((test_num >> 24) & 0xFF);
+    // buffer[1] = static_cast<uint8_t>((test_num >> 16) & 0xFF);
+    // buffer[2] = static_cast<uint8_t>((test_num >> 8) & 0xFF);
+    // buffer[3] = static_cast<uint8_t>(test_num & 0xFF);
+    // wiringPiSPIDataRW(spi_channel, buffer, 4);
     
+    // Set the pin mode
+    pinMode(sram_select0, OUTPUT);
+    pinMode(sram_select1, OUTPUT);
+    pinMode(inst_valid, INPUT);
+    pinMode(job_done, INPUT);
+    pinMode(execute_task, OUTPUT);
+
+    for (int i = 0; i < 50; i++) {
+        digitalWrite(sram_select0, LOW);
+        digitalWrite(sram_select1, LOW);
+        digitalWrite(execute_task, LOW);
+        delay(1000);
+
+        digitalWrite(sram_select0, LOW);
+        digitalWrite(sram_select1, LOW);
+        digitalWrite(execute_task, HIGH);
+        delay(1000);
+
+        digitalWrite(sram_select0, LOW);
+        digitalWrite(sram_select1, HIGH);
+        digitalWrite(execute_task, LOW);
+        delay(1000);
+
+        digitalWrite(sram_select0, LOW);
+        digitalWrite(sram_select1, HIGH);
+        digitalWrite(execute_task, HIGH);
+        delay(1000);
+
+        digitalWrite(sram_select0, HIGH);
+        digitalWrite(sram_select1, LOW);
+        digitalWrite(execute_task, LOW);
+        delay(1000);
+
+        digitalWrite(sram_select0, HIGH);
+        digitalWrite(sram_select1, LOW);
+        digitalWrite(execute_task, HIGH);
+        delay(1000);
+
+        digitalWrite(sram_select0, HIGH);
+        digitalWrite(sram_select1, HIGH);
+        digitalWrite(execute_task, LOW);
+        delay(1000);
+
+        digitalWrite(sram_select0, HIGH);
+        digitalWrite(sram_select1, HIGH);
+        digitalWrite(execute_task, HIGH);
+        delay(1000);
+    }
+
     return 0;
 }
