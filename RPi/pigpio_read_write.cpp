951bb4d7ffb4263f5e1a10513a2f7f7e687d8464
Justin S <justinshyi123@gmail.com>
2023-05-23 01:36:06 -0700

    debugging rpi pigpio_read_write attempt 1

--- RPi/pigpio_read_write.cpp
+++ RPi/pigpio_read_write.cpp
@@ -32,19 +32,19 @@ std::vector<char> readChar(int handle, int address, int length) {
 }
 
 // Function to write uint8_t to the SRAM chip.
-void writeRaw(int handle, int address, std::vector<uint8_t> data) {
+void writeData(int handle, int address, std::vector<uint8_t> data) {
     std::vector<uint8_t> buffer = {WRITE, (uint8_t)((address>>16)&0xFF), (uint8_t)((address>>8)&0xFF), (uint8_t)(address&0xFF)};
     buffer.insert(buffer.end(), data.begin(), data.end());
     
-    spiWrite(handle, &buffer[0], buffer.size());
+    spiWrite(handle, reinterpret_cast<char*>(&buffer[0]), buffer.size());
 }
 
 // Function to read uint8_t from the SRAM chip.
-std::vector<uint8_t> readRaw(int handle, int address, int length) {
+std::vector<uint8_t> readData(int handle, int address, int length) {
     std::vector<uint8_t> buffer = {READ, (uint8_t)((address>>16)&0xFF), (uint8_t)((address>>8)&0xFF), (uint8_t)(address&0xFF)};
     buffer.resize(4 + length);
 
-    spiXfer(handle, &buffer[0], &buffer[0], buffer.size());
+    spiXfer(handle, reinterpret_cast<char*>(&buffer[0]), reinterpret_cast<char*>(&buffer[0]), buffer.size());
 
     return std::vector<uint8_t>(buffer.begin() + 4, buffer.end());
 }
